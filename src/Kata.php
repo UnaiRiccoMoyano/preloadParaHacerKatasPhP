1e3b7b325f1b8c2ba54b1d62338820902c59f0f6
Unai.R <un4i37@gmail.com>
2020-02-17 12:46:03 +0100

    Primer commit

---
+++ src/Kata.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace factoriaF5;
+
+class Kata
+{   
+    
+}
+
---
+++ tests/KataTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use factoriaF5\Kata;
+
+use PHPUnit\Framework\TestCase;
+
+class KataTest extends TestCase
+{
+
+
+  public function test(){
+       
+
+      $this->assertEquals($expected,$response);
+  }
+
+}
